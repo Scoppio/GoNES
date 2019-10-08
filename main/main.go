405544baf0b3d8c40b6297a7a96ac781ed1e86dc
Lucas Coppio <scoppio@Giovannas-MacBook-Pro.local>
2019-10-08 01:21:59 -0300

    working on visualization

--- main/main.go
+++ main/main.go
@@ -2,27 +2,31 @@ package main
 
 import (
 	"fmt"
-	"image/color"
 	"math/rand"
 	"time"
 
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/imdraw"
 	"github.com/faiface/pixel/pixelgl"
+	"github.com/faiface/pixel/text"
+
 	"golang.org/x/image/colornames"
+	"golang.org/x/image/font/basicfont"
 )
 
 const (
-	height  = 240
-	width   = 256
+	height  = 480
+	width   = 680
 	rows    = 240
 	columns = 256
 )
 
 var (
-	bus    *Bus
-	frames = 0
-	second = time.Tick(time.Second)
+	bus      *Bus
+	basicTxt text
+	frames   = 0
+	second   = time.Tick(time.Second)
+	atlas    = text.NewAtlas(basicfont.Face7x13, text.ASCII)
 )
 
 func init() {
@@ -40,11 +44,6 @@ func main() {
 	pixelgl.Run(run)
 }
 
-type pix struct {
-	x, y, h, w int
-	color      *color.RGBA
-}
-
 func run() {
 	cfg := pixelgl.WindowConfig{
 		Title:  "GoNES",
@@ -59,19 +58,17 @@ func run() {
 
 	// last := time.Now()
 	for !win.Closed() {
-
-		var x, y, size float64 = 0, 0, 1
-		// colors := len(colornames.Names)
+		var x, y, size float64 = 0, 0, 2
 		i := 0
-		win.Clear(colornames.Black)
-		// max := int((x * y) / 4)
-		for y < rows {
-			for x < columns {
-				if rand.Float32() > 0.95 {
-					// imd.Color = colornames.Map[colornames.Names[int(x+y)%colors]]
-					imd.Color = colornames.Map[colornames.Names[int(x+y)%16]]
-					imd.Push(pixel.V(x, y), pixel.V(x+size, y+size))
-					imd.Rectangle(0)
+		// if frames%2 == 0 {
+		win.Clear(colornames.Blue)
+		// }
+		for y < rows*size {
+			for x < columns*size {
+				if rand.Float32() > 0.9 {
+					// imd.Color = colornames.Map[colornames.Names[int(x+y)%16]]
+					// imd.Push(pixel.V(x, y), pixel.V(x+1, y+1))
+					// imd.Rectangle(0)
 					i++
 				}
 				x += size
@@ -79,6 +76,8 @@ func run() {
 			x = 0
 			y += size
 		}
+
+		// basicTxt.Draw(win, pixel.IM)
 		imd.Draw(win)
 		imd.Clear()
 		win.Update()
@@ -89,10 +88,27 @@ func run() {
 			frames = 0
 		default:
 		}
-		// t2 := time.Now()
-		// diff := t2.Sub(t1)
-		// fmt.Println("Iterated over ", i, " cells and spent ", diff, " on that frame - ", 1.0/diff.Seconds(), " FPS")
-		// fps := 1.0 / diff.Seconds()
-		// fmt.Println(fps, "FPS")
+
 	}
+}
+
+func drawCpu(c *CPU6502) {
+	DrawString(0.0, 0.0, "Status")
+	DrawString(0.0+64, 0.0, fmt.Sprintln("N: ", c.StatusRegisterAsWord(N)))
+	DrawString(0.0+64*2, 0.0, fmt.Sprintln("V: ", c.StatusRegisterAsWord(V)))
+	DrawString(0.0+64*4, 0.0, fmt.Sprintln("U: ", c.StatusRegisterAsWord(U)))
+	DrawString(0.0+64*5, 0.0, fmt.Sprintln("B: ", c.StatusRegisterAsWord(B)))
+	DrawString(0.0+64*6, 0.0, fmt.Sprintln("D: ", c.StatusRegisterAsWord(D)))
+	DrawString(0.0+64*7, 0.0, fmt.Sprintln("I: ", c.StatusRegisterAsWord(I)))
+	DrawString(0.0+64*8, 0.0, fmt.Sprintln("Z: ", c.StatusRegisterAsWord(Z)))
+	DrawString(0.0+64*9, 0.0, fmt.Sprintln("C: ", c.StatusRegisterAsWord(C)))
+}
+
+func DrawString(x, y float64, message string) {
+	basicTxt = text.New(pixel.V(x, y), atlas)
+	// if color == nil {
+	// 	color = colornames.White
+	// }
+	basicTxt.Color = colornames.White
+	fmt.Fprintln(basicTxt, message)
 }
