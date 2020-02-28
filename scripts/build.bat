6dd0f72ad975eca0dd9323f92e0eaa4431c0a479
Lucas Coppio <lucascoppio@gmail.com>
2020-02-28 00:23:48 -0300

    working, the executable works too, needs to add functionality, work first on the unit tests

---
+++ scripts/build.bat
@@ -0,0 +1,1 @@
+go build -o ..\\output\\GoNES.exe ..\\internal\\Main.go ..\\internal\\Bus.go ..\\internal\\C6502.go ..\\internal\\P2C02.go ..\\internal\\DataTypes.go ..\\internal\\Utils.go ..\\internal\\Cartridge.go ..\\internal\\Mapper.go
---
+++ scripts/run.bat
@@ -0,0 +1,1 @@
+go run ..\\internal\\Main.go ..\\internal\\Bus.go ..\\internal\\C6502.go ..\\internal\\Cartridge.go ..\\internal\\DataTypes.go ..\\internal\\Mapper.go ..\\internal\\P2C02.go ..\\internal\\Utils.go
